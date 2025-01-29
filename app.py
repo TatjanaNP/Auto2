8d6448a90ff1bd831b4582f9a6de7b0b9a55233c
TatjanaNP <tatjananpetrova@outlook.com>
2025-01-29 10:01:06 +0200

    Pridėtas api endpointas su pydantic modeliu.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with app.app_context():
 
 
 @app.route("/api/automobiliai")
-def api_projektai():
+def api_automobiliai():
     all_cars = Automobilis.query.all()
     car_data = [{
         "id": car.id,
@@ -27,6 +27,12 @@ def api_projektai():
 
     return jsonify(car_data)
 
+
+@app.route("/api2/automobiliai")
+def api2_automobiliai():
+    all_cars = Automobilis.query.all()
+    car_data = [ProjektasSchema.model_validate(car).model_dump() for car in all_cars]
+    return jsonify(car_data)
 
 
 if __name__ == "__main__":
